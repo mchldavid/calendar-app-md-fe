538d9591482c825c7a25b68017cd1fde89874292
mDavid <david.michael15.md@gmail.com>
2023-05-13 17:38:53 +0800

    Test Api's using Axios from mock REST API

--- src/api/calendarAppApi.js
+++ src/api/calendarAppApi.js
@@ -9,18 +9,82 @@ const calendarAppApi = axios.create({
   },
 })
 
-// Login
+//POST Login
 export const loginApi = async (user) => {
   try {
-    const response = await calendarAppApi
-      .post("/login", {
-        email: user.email,
-        password: user.password,
-      })
+    const response = await calendarAppApi.post("/login", {
+      email: user.email,
+      password: user.password,
+    })
     console.log(response.data)
     return response.data
   } catch (error) {
-    console.error("Error fetching users:", error)
+    console.error("Error Login:", error)
+  }
+}
+
+// GET all Appointments
+export const getAppointments = async () => {
+  try {
+    const response = await calendarAppApi.get("/appointment")
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.error("Error Fetching appointments:", error)
+  }
+}
+
+// POST Create new
+export const createAppointment = async (detail) => {
+  try {
+    const response = await calendarAppApi.post("/appointment", {
+      name: detail.name,
+      date: detail.date,
+      status: detail.status,
+    })
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.error("Error in Create New:", error)
+  }
+}
+
+// PUT Edit details
+export const editAppointment = async (detail) => {
+  try {
+    const response = await calendarAppApi.put(`/appointment/${detail.id}`, {
+      name: detail.name,
+      date: detail.date,
+      status: detail.status,
+    })
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.error("Error in Edit Appointment:", error)
+  }
+}
+
+// PATCH Edit details
+export const toggleStatusAppointment = async (detail) => {
+  try {
+    const response = await calendarAppApi.patch(`/appointment/${detail.id}`, {
+      status: detail.status,
+    })
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.error("Error in toggle status Appointment:", error)
+  }
+}
+
+// DELETE Appointment
+export const deleteAppointment = async (detail) => {
+  try {
+    const response = await calendarAppApi.delete(`/appointment/${detail.id}`)
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.error("Error in Deleting Appointment:", error)
   }
 }
 
--- src/pages/TestApi.js
+++ src/pages/TestApi.js
@@ -1,26 +1,105 @@
 import React, { useState } from "react"
-import { loginApi } from "../api/calendarAppApi"
+import {
+  loginApi,
+  getAppointments,
+  createAppointment,
+  editAppointment,
+  toggleStatusAppointment,
+  deleteAppointment,
+} from "../api/calendarAppApi"
 
 const TestApi = () => {
   const [resLoginApi, setResLoginApi] = useState()
+  const [output, setOutput] = useState()
+  const [toggle, setToggle] = useState("pending")
 
-  const testLogin = async (n) => {
-    if (n === 1) {
-      const getResponse = await loginApi({
-        email: "guest@email.com",
-        password: "Pass123",
-      })
-      console.log("API LOGIN: ", getResponse)
-      setResLoginApi(getResponse)
+  const fetch = async (n) => {
+    var getResponse
+    switch (n) {
+      case 1:
+        getResponse = await loginApi({
+          email: "guest@email.com",
+          password: "Pass123",
+        })
+        console.log("API LOGIN: ", getResponse)
+        setResLoginApi(getResponse)
+        break
+
+      case 2:
+        getResponse = await getAppointments()
+        console.log("API Get appointments: ", getResponse)
+        setOutput(getResponse)
+        break
+
+      case 3:
+        getResponse = await createAppointment({
+          name: "Meeting 1",
+          date: "2023-12-01",
+          status: "pending",
+        })
+        console.log("API Create New: ", getResponse)
+        setOutput(getResponse)
+        break
+
+      case 4:
+        getResponse = await editAppointment({
+          id: "1",
+          name: "Tobi",
+          date: "2023-12-01",
+          status: "completed",
+        })
+        console.log("API Edit: ", getResponse)
+        setOutput(getResponse)
+        break
+
+      case 5:
+        getResponse = await toggleStatusAppointment({
+          id: "1",
+          status: toggle === "pending" ? "completed" : "pending",
+        })
+        setToggle(getResponse.status)
+
+        console.log("API Toggle: ", getResponse)
+        setOutput(getResponse)
+        break
+
+      case 6:
+        getResponse = await deleteAppointment({
+          id: "1",
+        })
+        setToggle(getResponse.status)
+
+        console.log("API Delete Appointment: ", getResponse)
+        setOutput(getResponse)
+        break
+      default:
     }
   }
 
   return (
     <>
-      <div className="w-full h-full flex justify-start items-start">
+      <div className="w-full h-full flex justify-start items-start flex-col">
         <div>
-          <button onClick={() => testLogin(1)}>Test Login Api</button>{" "}
+          <button onClick={() => fetch(1)}>Test Login Api</button>{" "}
           <pre>{JSON.stringify(resLoginApi)}</pre>
+        </div>
+        <div>
+          <button onClick={() => fetch(2)}>Test Get Appointments</button>
+        </div>
+        <div>
+          <button onClick={() => fetch(3)}>Test Create Appointment</button>
+        </div>
+        <div>
+          <button onClick={() => fetch(4)}>Test Edit Appointment</button>
+        </div>
+        <div>
+          <button onClick={() => fetch(5)}>Test Toggle Appointment</button>
+        </div>
+        <div>
+          <button onClick={() => fetch(6)}>Test Delete Appointment</button>
+        </div>
+        <div className="bg-c4 text-c2 p-3 w-full break-word">
+          <div className="break-all">{JSON.stringify(output)}</div>
         </div>
       </div>
     </>
